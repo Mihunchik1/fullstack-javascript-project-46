fdf927e8c66eba86d361a0062bd58927f9dd8a60
Mihunchik <arinushkin048@mail.ru>
2024-10-04 13:58:55 +0300

    add eslint without airbnb

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import globals from "globals";
+
+
+export default [
+  {
+    files: ['*.js'],
+    languageOptions: { globals: globals.node },
+    plugins: {
+      // Добавь нужные плагины
+    },
+    rules: {
+      // Настрой свои правила
+    },
+  },
+];
