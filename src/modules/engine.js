9d9471cb6283c23bf85bb6dbfeaab92cfff5cfb4
Mihunchik <arinushkin048@mail.ru>
2024-12-07 12:17:31 +0300

    change hasOwnProperty and hasOwn

--- src/modules/engine.js
+++ src/modules/engine.js
@@ -2,10 +2,10 @@ export default (obj1, obj2) => {
   const iter = (firstObj, secondObj) => {
     const keys = [...new Set([...Object.keys(firstObj), ...Object.keys(secondObj)])].sort();
     return keys.map((key) => {
-      if (!Object.hasOwn(firstObj, key)) {
+      if (!firstObj.hasOwnProperty(key)) {
         return { type: 'added', key, value: secondObj[key] };
       }
-      if (!Object.hasOwn(secondObj, key)) {
+      if (!secondObj.hasOwnProperty(key)) {
         return { type: 'removed', key, value: firstObj[key] };
       }
       if (typeof firstObj[key] === 'object' && typeof secondObj[key] === 'object' && firstObj[key] !== null && secondObj[key] !== null) {
